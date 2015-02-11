fc568a6996689f4cc1e80cafa54f2a7714cd608a
Danielle <danielle@daniellemullane.com>
2015-02-11 13:59:51 -0700

    create test for editing location

--- spec/features/location_spec.rb
+++ spec/features/location_spec.rb
@@ -49,7 +49,34 @@ describe 'User can CRUD locations' do
   end
 
   scenario 'User can edit a location' do
-    #fill in
+
+        # visit root
+        visit '/'
+
+        # click on link to go to new location form
+        click_on "New Location"
+
+        #filling out form for location
+        fill_in 'location[name]', :with => "Home"
+        fill_in 'location[address]', with: "New Address"
+        fill_in 'location[zipcode]', with: "90210"
+
+        #submitting form to create a location
+        click_on "Create Location"
+
+        click_on "Edit"
+
+        fill_in 'location[name]', :with => "Store"
+
+        click_on "Update Location"
+
+        expect(page).to have_content("Store")
+        expect(page).to have_content("New Address")
+        expect(page).to have_content("90210")
+
+
+
+
   end
 
 
