d243957df926df5e8715c489dd816c44636e2645
Danielle <danielle@daniellemullane.com>
2015-02-11 14:03:22 -0700

    create test for deleting location

--- spec/features/location_spec.rb
+++ spec/features/location_spec.rb
@@ -76,12 +76,31 @@ describe 'User can CRUD locations' do
 
 
 
-
   end
 
 
   scenario 'User can delete a location' do
-    #fill in
+    # visit root
+    visit '/'
+
+    # click on link to go to new location form
+    click_on "New Location"
+
+    #filling out form for location
+    fill_in 'location[name]', :with => "Home"
+    fill_in 'location[address]', with: "New Address"
+    fill_in 'location[zipcode]', with: "90210"
+
+    #submitting form to create a location
+    click_on "Create Location"
+
+    click_on "Delete"
+
+    expect(page).to have_no_content("Store")
+    expect(page).to have_no_content("New Address")
+    expect(page).to have_no_content("90210")
+
+
   end
 
 
