e9aca225fd7a8979d8f7cc4a3afc685ef0911596
Danielle <danielle@daniellemullane.com>
2015-02-11 14:27:10 -0700

    Create test to see if users can edit an event

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -27,7 +27,34 @@ describe 'User can CRUD locations' do
       expect(page).to have_content("School")
       expect(page).to have_content("2015-01-01")
       expect(page).to have_content("true")
+  end
 
+  scenario 'Users can edit an event.' do
+    # visit root
+    visit '/'
 
+    # click on link to go to new location form
+    click_on "New Location"
+
+    #filling out form for location
+    fill_in 'location[name]', :with => "Galvanize"
+    fill_in 'location[address]', with: "New Address"
+    fill_in 'location[zipcode]', with: "90210"
+
+    #submitting form to create a location
+    click_on "Create Location"
+
+    click_on "Add Event"
+
+    fill_in 'event[description]',:with => "School"
+    fill_in 'event[date]', :with => "01/01/2015"
+    check 'event[requires_id]'
+    click_on "Create Event"
+    click_on "Edit"
+    fill_in 'event[description]', :with => "School2"
+    click_on "Update Event"
+
+    expect(page).to have_content("School2")
+    
   end
 end
