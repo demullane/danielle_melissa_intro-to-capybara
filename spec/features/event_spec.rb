38cb1f9d0d188e925332c534fa950610f488a815
Danielle <danielle@daniellemullane.com>
2015-02-11 14:30:59 -0700

    Create test to see if users can delete an event

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -55,6 +55,33 @@ describe 'User can CRUD locations' do
     click_on "Update Event"
 
     expect(page).to have_content("School2")
-    
+
+  end
+
+  scenario 'Users can delete an event.' do
+    # visit root
+    visit '/'
+
+    # click on link to go to new location form
+    click_on "New Location"
+
+    #filling out form for location
+    fill_in 'location[name]', :with => "Galvanize"
+    fill_in 'location[address]', with: "New Address"
+    fill_in 'location[zipcode]', with: "90210"
+
+    #submitting form to create a location
+    click_on "Create Location"
+
+    click_on "Add Event"
+
+    fill_in 'event[description]',:with => "School"
+    fill_in 'event[date]', :with => "01/01/2015"
+    check 'event[requires_id]'
+    click_on "Create Event"
+
+    click_on "Delete"
+
+    expect(page).to have_no_content("School2")
   end
 end
